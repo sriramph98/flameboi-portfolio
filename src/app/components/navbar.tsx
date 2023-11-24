3090862273aacc606cc92d27146c45c35473528c
Sriram P H <sriramh9812@gmail.com>
2023-11-24 06:15:31 -0500

    commit

---
+++ src/app/components/navbar.tsx
@@ -0,0 +1,13 @@
+export default function NavBar() {
+    return (
+    <div>
+        <ul>
+            Work
+        </ul>
+        <ul>
+            Marketplace
+        </ul>
+    </div>
+    )
+  }
+  
