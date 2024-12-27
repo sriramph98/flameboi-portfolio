ffa3cf500b022658987bdd9e16b65291e7b83f17
Sriram P H <sriramph9812@gmail.com>
2024-12-27 04:42:06 -0500

    added env

--- src/lib/airtable.ts
+++ src/lib/airtable.ts
@@ -1,5 +1,13 @@
 import Airtable from "airtable";
 
+if (!process.env.AIRTABLE_API_KEY) {
+  throw new Error("AIRTABLE_API_KEY is not defined");
+}
+
+if (!process.env.AIRTABLE_BASE_ID) {
+  throw new Error("AIRTABLE_BASE_ID is not defined");
+}
+
 // Initialize Airtable client
 const airtable = new Airtable({
   apiKey: process.env.AIRTABLE_API_KEY,
